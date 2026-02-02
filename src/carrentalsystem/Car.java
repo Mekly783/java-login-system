c4ac3ab607e856851389779cfc935f95c6b6bdf0
Everlyne Maina <everlynemaina05@gmail.com>
2026-02-02 16:08:53 +0300

    Update Car.java

--- src/carrentalsystem/Car.java
+++ src/carrentalsystem/Car.java
@@ -1,39 +1,55 @@
 package carrentalsystem;
+/*
+ * Car.java
+ * This class represents a car in the Car Rental System.
+ * It stores car details and manages renting and returning of cars.
+ */
 
 public class Car {
 
+    // Unique identifier for the car
     private String carId;
-    private String model;
-    private boolean isAvailable;
 
-    // Constructor
+    // Model name of the car
+    private String model;
+
+    // Availability status of the car
+    private boolean available;
+
+    // Constructor to initialize car details
     public Car(String carId, String model) {
         this.carId = carId;
         this.model = model;
-        this.isAvailable = true;
+        this.available = true; // Car is available by default
     }
 
-    // Rent the car
-    public void rentCar() {
-        isAvailable = false;
-    }
-
-    // Return the car
-    public void returnCar() {
-        isAvailable = true;
-    }
-
-    // Getters
+    // Method to get car ID
     public String getCarId() {
         return carId;
     }
 
+    // Method to get car model
     public String getModel() {
         return model;
     }
 
+    // Method to check if car is available
     public boolean isAvailable() {
-        return isAvailable;
+        return available;
+    }
+
+    // Method to rent the car
+    public boolean rentCar() {
+        if (available) {
+            available = false;
+            return true;
+        }
+        return false;
+    }
+
+    // Method to return the car
+    public void returnCar() {
+        available = true;
     }
 }
 
