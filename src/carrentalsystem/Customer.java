66d72d0f3b52f2718072659fd87e1a010d7db431
Everlyne Maina <everlynemaina05@gmail.com>
2026-02-02 16:09:28 +0300

    Enhance documentation in Customer class
    Added class-level comments to enhance code documentation.

--- src/carrentalsystem/Customer.java
+++ src/carrentalsystem/Customer.java
@@ -1,22 +1,32 @@
 package carrentalsystem;
+/*
+ * Customer.java
+ * This class represents a customer in the Car Rental System.
+ * It stores customer information.
+ */
 
 public class Customer {
 
+    // Unique customer ID
     private String customerId;
+
+    // Name of the customer
     private String name;
 
-    // Constructor
+    // Constructor to initialize customer details
     public Customer(String customerId, String name) {
         this.customerId = customerId;
         this.name = name;
     }
 
-    // Getters
+    // Method to get customer ID
     public String getCustomerId() {
         return customerId;
     }
 
+    // Method to get customer name
     public String getName() {
         return name;
     }
 }
+
